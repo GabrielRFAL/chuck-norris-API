f1d9894df8a9aabb8e0e49f745e3b892b263ec61
Gabriel Ribas Ferreira de Almeida <gabrielribasfal@gmail.com>
2025-05-26 12:40:24 -0300

    feat: adicão da piada no modal

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,10 +4,12 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
   imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
+
 export class HomeComponent {
 
   private modalService = inject(NgbModal);
@@ -16,8 +18,8 @@ export class HomeComponent {
     const modalRef = this.modalService.open(ModalHomeComponent);
     modalRef.componentInstance.valor = 'value';
     modalRef.componentInstance.imagem = 'https://api.chucknorris.io/img/avatar/chuck-norris.png';
-    
-
   }
+
+  
 
 }
--- src/app/components/modal-home/modal-home.component.ts
+++ src/app/components/modal-home/modal-home.component.ts
@@ -1,25 +1,53 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ChuckApiService } from '../../services/chuck-api.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-modal-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './modal-home.component.html',
   styleUrl: './modal-home.component.scss'
 })
-export class ModalHomeComponent {
 
-  private modalService = inject(NgbModal);
+export class ModalHomeComponent implements OnInit {
+
+  constructor(
+    private modalService: NgbModal,
+    private chuckService: ChuckApiService
+  ) {}
+
   valor = '';
   imagem = '';
+  piada = '';
+  isLoading = false;
+  errorMessage = '';
 
-  closeModal():void {
-    this.modalService.dismissAll()
+  closeModal(): void {
+    this.modalService.dismissAll();
   }
 
-  ngOnInit(){
-    console.warn("valor recebido ==> ", this.valor)
-    console.warn("valor recebido ==> ", this.imagem)
+  ngOnInit(): void {
+    console.warn("valor recebido ==> ", this.valor);
+    console.warn("valor recebido ==> ", this.imagem);
+
+    this.fetchJoke();
   }
 
+  fetchJoke(): void {
+    this.isLoading = true;
+    this.errorMessage = '';
+    this.chuckService.getJoke().subscribe({
+      next: (data) => {
+        this.piada = data.value;
+        this.isLoading = false;
+      },
+      error: (err) => {
+        console.error('Erro ao buscar piada:', err);
+        this.errorMessage = 'Erro ao carregar piada.';
+        this.isLoading = false;
+      }
+    });
+  }
 }
